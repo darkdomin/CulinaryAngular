134bc48d325437b7b34bcf5803485eda333b6c2e
darkdomin <darkdomin@wp.pl>
2022-02-22 18:04:27 +0100

    new method - getLastes and detectScreenSize
    getLastes - set the amount of recipes on the home page.

--- src/app/recipes/home/home.component.ts
+++ src/app/recipes/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import * as _ from 'lodash';
 import { Recipe } from '../models/recipe';
 import { RecipesService } from '../recipes.service';
 
@@ -11,6 +12,7 @@ import { RecipesService } from '../recipes.service';
 export class HomeComponent implements OnInit {
 
   recipes!: Recipe[];
+  shortRecipes!: Recipe[];
 
   constructor(
     private recipeService: RecipesService,
@@ -18,16 +20,36 @@ export class HomeComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.loadRecipes();
+    this.loadRecipes(9);
   }
 
-  loadRecipes(): void {
+  @HostListener("window:resize") onResize() {
+    if(this.detectScreenSize() < 1200){
+      this.loadRecipes(8);
+    }
+    else{
+      this.loadRecipes(9);
+    }
+  }
+
+  loadRecipes(amount: number): void {
     this.recipeService.getRecipes().subscribe((recipes) => {
     this.recipes = recipes;
+    this.getLastest(amount);
     });
   }
 
   goToRecipeDetails(recipe: Recipe){
     this.router.navigate(['/recipes', recipe?.id]);
+  }
+
+  getLastest(amount: number): void {
+    this.recipes = _.takeRight(this.recipes, amount);
+  }
+
+  private detectScreenSize():number {
+    const width =  window.innerWidth;
+    console.log(width);
+    return width;
   }
 }
